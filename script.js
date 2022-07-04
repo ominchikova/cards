3e6e90770591117c14cd4ffc76da6aa8be1b5be9
tatyanakoroliova130284 <tatyana.koroliova@gmail.com>
2022-07-04 20:51:06 +0300

    answer

---
+++ script.js
@@ -0,0 +1,48 @@
+const getAnswer = (index) => {
+  const answers = ["1", "2", "3", "4", "5"];
+  return answers[index];
+};
+const cards = [
+  {
+    input: document.querySelector("#input1"),
+    btn: document.querySelector(".btn1"),
+  },
+  {
+    input: document.querySelector("#input2"),
+    btn: document.querySelector(".btn2"),
+  },
+  {
+    input: document.querySelector("#input3"),
+    btn: document.querySelector(".btn3"),
+  },
+  {
+    input: document.querySelector("#input4"),
+    btn: document.querySelector(".btn4"),
+  },
+  {
+    input: document.querySelector("#input5"),
+    btn: document.querySelector(".btn5"),
+  },
+];
+
+let counter = 0;
+cards
+  .sort(() => Math.random() - 0.5)
+  .forEach((card, index) => {
+    card.btn.addEventListener("click", handlerChekAnswer.bind(card, index));
+  });
+
+function handlerChekAnswer(index) {
+  if (this.input.value === getAnswer(index)) {
+    this.input.disabled = true;
+    this.btn.disabled = true;
+    counter++;
+    if (counter === 5) {
+      alert("Вы ответили верно");
+      window.location.href = "http://google.com";
+    }
+  } else {
+    this.input.value = "";
+    alert("Ответ неверный, попробуй еще!");
+  }
+}
